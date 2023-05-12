563f71bdd00482e00d13168d5fb3d8c4f4914dd1
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-12 11:09:56 +0200

    Test existing methods before refactoring

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    @Test
+    public void testStatus() {
+        TodoItem item = new TodoItem("Walk dog", "Take the dog for a walk around the park", "Incomplete");
+        Assertions.assertEquals("Incomplete", item.getStatus());
+
+        item.setStatus("Complete");
+        Assertions.assertEquals("Complete", item.getStatus());
+    }
+
+
 }
