5173964cbfc87d6c4f3241a752ae51c98bb62cfb
Dennis <dennisvoutos@gmail.com>
2023-05-12 11:49:24 +0300

    Get title failed becuase it is not ready yet

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -9,4 +9,9 @@ class TodoItemTest {
         TodoItem item = new TodoItem("Go to the doctor","Go for my annual appointment","Not done");
         Assertions.assertEquals(item.getStatus(),"Not done"); // the get status works
     }
+    @Test
+    public void testGetTitle(){
+        TodoItem item = new TodoItem("Go to the doctor","Go for my annual appointment","Not done");
+        Assertions.assertEquals(item.getTitle(),"Go to the doctor"); // should fail because the function is not implemented
+    }
 }
