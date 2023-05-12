3647719a45fabfb59a647df0a36c5db488db9c8d
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-12 13:34:39 +0300

    test Account enabled state

--- src/test/java/com/booleanuk/core/UserManagerTest.java
+++ src/test/java/com/booleanuk/core/UserManagerTest.java
@@ -19,4 +19,19 @@ public class UserManagerTest {
         Assertions.assertFalse(userManager.createUser("app.com", "123456789"));
         Assertions.assertFalse(userManager.createUser("mpampis@app.com", "1234567"));
     }
+
+    @Test
+    public void testAccountDisabledByDefault() {
+        userManager.createUser("mpampis@app.com", "123456789");
+        Assertions.assertFalse(userManager.getUsers().get(0).isEnabled());
+    }
+
+    @Test
+    public void testAccountEnabledState() {
+        userManager.createUser("mpampis@app.com", "123456789");
+        Assertions.assertFalse(userManager.canLogin("mpampis@app.com"));
+
+        userManager.getUsers().get(0).setEnabled(true);
+        Assertions.assertTrue(userManager.canLogin("mpampis@app.com"));
+    }
 }
