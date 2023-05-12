c4f1a95bd9a24976d323d894eb2d70a95a781e95
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-12 13:27:35 +0300

    Implemented createUser with validations

--- src/main/java/com/booleanuk/core/UserManager.java
+++ src/main/java/com/booleanuk/core/UserManager.java
@@ -21,4 +21,18 @@ public class UserManager {
     public void setUsers(List<User> users) {
         this.users = users;
     }
+
+    public boolean createUser(String mail, String password) {
+        if (!mail.contains("@")) {
+            System.out.println("Email is not valid");
+            return false;
+        }
+        if (password.length() < 8) {
+            System.out.println("Password is too short");
+            return false;
+        }
+
+        users.add(new User(mail, password));
+        return true;
+    }
 }
