203f24049eb481a1780bf644d2d423126e3acf0a
Farhang Younis <comp-sci@hotmail.com>
2023-05-12 12:22:30 +0200

    Testing getEmail() without method implementation

---
+++ src/main/java/com/booleanuk/core/UserAccount.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+public class UserAccount {
+    private String email;
+    private String password;
+    private boolean enabled;
+
+    public UserAccount(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.enabled=false;
+    }
+
+}
---
+++ src/test/java/com/booleanuk/core/UserAccountTest.java
@@ -0,0 +1,13 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserAccountTest {
+    @Test
+    public void testSetEmail(){
+        UserAccount uAccount = new UserAccount("anything@gmail.com","any");
+
+        Assertions.assertEquals("anything@gmail.com",uAccount.getEmail());
+    }
+}
