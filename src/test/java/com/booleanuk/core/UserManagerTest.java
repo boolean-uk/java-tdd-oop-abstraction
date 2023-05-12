301065aa77c0d604cc48f49112256ace31c642db
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-12 13:23:50 +0300

    test Valid Email and Password

--- src/test/java/com/booleanuk/core/UserManagerTest.java
+++ src/test/java/com/booleanuk/core/UserManagerTest.java
@@ -1,6 +1,8 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class UserManagerTest {
 
@@ -9,5 +11,12 @@ public class UserManagerTest {
     @BeforeEach
     public void setup(){
         userManager = new UserManager();
+    }
+
+    @Test
+    public void testValidEmailAndPassword() {
+        Assertions.assertTrue(userManager.createUser("mpampis@app.com", "123456789"));
+        Assertions.assertFalse(userManager.createUser("app.com", "123456789"));
+        Assertions.assertFalse(userManager.createUser("mpampis@app.com", "1234567"));
     }
 }
