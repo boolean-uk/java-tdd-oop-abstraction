866487a7210a1930f0add2836182d154e4a88591
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-12 12:04:21 +0200

    Refactor TodoItem

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -3,12 +3,17 @@ package com.booleanuk.core;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class TodoItem {
     private final String title;
     private final String detail;
     private Status status;
+
+    public TodoItem(String title, String detail) {
+        this.title = title;
+        this.detail = detail;
+        this.status = Status.InProgress;
+    }
 
     public void toggleStatus() {
         status = switch (status) {
