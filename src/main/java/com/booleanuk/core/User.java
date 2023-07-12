11a9c802636188e2f42d6c85a312cfef0ad73143
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-12 12:38:00 +0200

    green stage of testCreateUser_WhenEmailIsInvalid_ShouldReturnProperMessage

--- src/main/java/com/booleanuk/core/User.java
+++ src/main/java/com/booleanuk/core/User.java
@@ -4,6 +4,8 @@ public class User {
     public String createUser(String email, String password) {
         if(email.contains("@") && password.length() >= 8){
             return "Account successfully created!";
+        } else if(!email.contains("@")){
+            return "Account not created! Invalid email!";
         }
 
         return "";
