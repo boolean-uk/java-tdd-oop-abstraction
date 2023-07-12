ccfbf8be92aeed276bb50b681ff3514af790be08
karim <01171252@pw.edu.pl>
2023-07-12 12:08:13 +0200

    getters and setters

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -1,8 +1,32 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @BeforeEach
+    void init() {
+        TodoItem todoItem = new TodoItem();
+    }
+    TodoItem todoItem = new TodoItem();
+    @Test
+    public void testGetSetDetails(){
+        todoItem.setDetail("aaa");
+        Assertions.assertEquals("aaa", todoItem.getDetail());
+    }
+
+    @Test
+    public void testGetSetTitle(){
+        todoItem.setTitle("bbb");
+        Assertions.assertEquals("bbb", todoItem.getTitle());
+    }
+
+    @Test
+    public void testGetSetStatus(){
+        todoItem.setStatus("ccc");
+        Assertions.assertEquals("ccc", todoItem.getStatus());
+    }
+
 
 }
