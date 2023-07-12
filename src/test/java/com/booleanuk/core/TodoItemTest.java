b932a31fc54724fd2d0a31d82db01ae8b55be161
Julia Girejko <julia.girejko@talan.com>
2023-07-12 12:09:49 +0200

    exc 1

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void statusSetGetTest() {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setStatus("active");
+        Assertions.assertEquals("active", todoItem.getStatus());
+    }
+
+    @Test
+    public void titleSetGetTest() {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTitle("Hobbit");
+        Assertions.assertEquals("Hobbit", todoItem.getTitle());
+    }
+
+    @Test
+    public void detailSetGetTest() {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setDetail("First detail");
+        Assertions.assertEquals("First detail", todoItem.getDetail());
+    }
 
 }
