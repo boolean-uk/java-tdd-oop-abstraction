2b8505689e3f4e16a552bf09e7d14850171769c2
Anastasiya <ronas0709@gmail.com>
2023-07-12 12:06:51 +0200

    added tests

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    void testSetTitle(){
+        TodoItem item = new TodoItem("Title", "Detail","Status");
+        item.setTitle("New Title");
+        Assertions.assertEquals("New Title", item.getTitle());
+    }
+    @Test
+    void testSetDetail(){
+        TodoItem item = new TodoItem("Title", "Detail","Status");
+        item.setDetail("New Detail");
+        Assertions.assertEquals("New Detail", item.getDetail());
+    }
+    @Test
+    void testSetStatus(){
+        TodoItem item = new TodoItem("Title", "Detail","Status");
+        item.setStatus("Completed");
+        Assertions.assertEquals("Completed", item.getStatus());
+    }
+
 
 }
