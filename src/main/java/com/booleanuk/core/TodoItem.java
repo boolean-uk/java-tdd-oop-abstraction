7c0e96df7a7614fd04d1662e9d734eb69ef6b6e5
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-12 12:07:56 +0200

    Solved exercise

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -1,14 +1,14 @@
 package com.booleanuk.core;
 
 public class TodoItem {
-    public String title;
-    public String detail;
-    public String status;
+    private String title;
+    private String detail;
+    private String status;
 
     public TodoItem(String title, String detail, String status) {
-        this.title = title;
-        this.detail = detail;
-        this.status = status;
+        this.setTitle(title);
+        this.setDetail(detail);
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
@@ -17,5 +17,21 @@ public class TodoItem {
 
     public String getStatus() {
         return this.status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
     }
 }
