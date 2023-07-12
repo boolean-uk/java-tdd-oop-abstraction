09ac160a9f6195e2447908bd2a6fcbbfb567bc50
Michał Klukowski <michal.klukowski@talan.com>
2023-07-12 12:39:09 +0200

    Implement UserDb and User classes

---
+++ src/main/java/com/booleanuk/core/User.java
@@ -0,0 +1,36 @@
+package com.booleanuk.core;
+
+public class User {
+    private String email;
+    private String passwordHash;
+    private boolean active;
+
+    public User(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
