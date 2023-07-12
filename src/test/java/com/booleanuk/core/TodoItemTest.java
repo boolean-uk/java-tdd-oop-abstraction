9f62d8454c369cf9b11dab2a91f0ccb158db5958
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-12 12:08:51 +0200

    Created tests for getters and setters

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,23 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    @Test
+    public void gettersTest(){
+        TodoItem todoItem = new TodoItem("New item","Detailed description","Finished");
+        Assertions.assertEquals("New item", todoItem.getTitle());
+        Assertions.assertEquals("Detailed description", todoItem.getDetail());
+        Assertions.assertEquals("Finished", todoItem.getStatus());
+    }
+
+    @Test
+    public void settersTest(){
+        TodoItem todoItem = new TodoItem("New item","Detailed description","Finished");
+        todoItem.setTitle("Go shopping");
+        todoItem.setDetail("Buy bread");
+        todoItem.setStatus("To do");
+        Assertions.assertEquals("Go shopping", todoItem.getTitle());
+        Assertions.assertEquals("Buy bread", todoItem.getDetail());
+        Assertions.assertEquals("To do", todoItem.getStatus());
+    }
+
 }
