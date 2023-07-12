1bc4af6b9dd0fa64e8c9e9999171a6ee5059393b
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-12 12:44:13 +0200

    created classes for user admin and account

---
+++ src/main/java/com/booleanuk/core/User.java
@@ -0,0 +1,13 @@
+package com.booleanuk.core;
+
+public class User {
+    private Account account;
+
+    public void createAccount(String email, String password) {
+        this.account = new Account(email, password);
+    }
+
+    public boolean canLogIn() {
+        return this.account.getStatus().equals("Enabled");
+    }
+}
