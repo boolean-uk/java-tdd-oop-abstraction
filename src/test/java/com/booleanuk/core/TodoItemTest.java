11710eeec13f45141439e9d2df223618fb50f53e
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-12 12:04:16 +0200

    Add test cases for TodoItem

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -1,8 +1,23 @@
 package com.booleanuk.core;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TodoItemTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class TodoItemTest {
+    @Test
+    void constructor_shouldInitializeTodoItemWithCorrectStatus() {
+        var todoItem = new TodoItem("todo", "important task");
+
+        assertEquals(TodoItem.Status.InProgress, todoItem.getStatus());
+    }
+
+    @Test
+    void toggleStatus_shouldCorrectlyToggleStatus() {
+        var todoItem = new TodoItem("todo", "important task");
+
+        todoItem.toggleStatus();
+
+        assertEquals(TodoItem.Status.Done, todoItem.getStatus());
+    }
 }
