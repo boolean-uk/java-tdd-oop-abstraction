a98934345f16418fe949622648ed1edb0ea40006
Martyna <martyna.marszalek@talan.com>
2023-07-12 12:26:49 +0200

    Test for setEmail

--- src/test/java/com/booleanuk/core/UserAccountTest.java
+++ src/test/java/com/booleanuk/core/UserAccountTest.java
@@ -1,5 +1,35 @@
 package com.booleanuk.core;
 
-public class UserAccountTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class UserAccountTest {
+    UserAccount userAccount;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp(){
+        userAccount = new UserAccount();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void setEmailTest(){
+        userAccount.setEmail("malpa@com");
+        Assertions.assertEquals("malpa@com", userAccount.getEmail());
+        userAccount.setEmail("malpa");
+        Assertions.assertNotEquals("malpa@com",userAccount.getEmail());
+        Assertions.assertEquals("Email not contains @", outputStreamCaptor.toString().trim());
+    }
 }
