11eb21f7bf2721f6584945c5cd5a15a06056d0f4
himok <himok.pk@gmail.com>
2023-07-12 12:11:08 +0200

    Adding getters and setters

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -1,21 +1,40 @@
 package com.booleanuk.core;
 
 public class TodoItem {
-    public String title;
-    public String detail;
-    public String status;
+    private String title;
+    private String detail;
+    private String status;
 
-    public TodoItem(String title, String detail, String status) {
+    public TodoItem(String title, String detail) {
         this.title = title;
         this.detail = detail;
-        this.status = status;
+        this.status = "InCompleted";
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setStatusCompleted() {
+        this.status = "Completed";
+    }
+    public void setStatusInCompleted() {
+        this.status = "InCompleted";
     }
 
     public String getStatus() {
         return this.status;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
