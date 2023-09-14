0172ef51d221a8ded61568e774218892d1e23af1
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-09-14 12:12:39 +0300

    Panagiotis Karapiperis ex1

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void testAbstraction(){
+        TodoItem todo = new TodoItem("Wash the clothes", "seperate colours","not completed");
+        todo.setTitle("clean the floor");
+        todo.setDetail("attention to the corners");
+        todo.setStatus("completed");
+        Assertions.assertEquals(todo.getTitle(),"clean the floor");
+        Assertions.assertEquals(todo.getDetail(),"attention to the corners");
+        Assertions.assertEquals(todo.getStatus(),"completed");
+    }
 
 }
