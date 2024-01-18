3b7da718f4527e2860dd52e0f1ecac0b31d1f508
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-18 09:40:53 +0100

    Make account pass tests

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -11,11 +11,11 @@ public class AccountTest {
 
         Assertions.assertEquals("invalid password", account.setPassword("1"));
         Assertions.assertEquals("invalid email", account.setEmail("1"));
-        Assertions.assertFalse(account.getEnabled());
+        Assertions.assertFalse(account.isEnabled());
         Assertions.assertEquals("invalid login", account.ableToLogin());
         account.setEnabled(true);
-        Assertions.assertTrue(account.getEnabled());
-        Assertions.assertTrue(account.ableToLogin());
+        Assertions.assertTrue(account.isEnabled());
+        Assertions.assertEquals("valid login", account.ableToLogin());
 
 
 
