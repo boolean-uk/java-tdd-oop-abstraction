6ede3771cf6e58a4801b4a21e9cbaada579e3b32
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-18 09:17:43 +0100

    Add init test for Account, User, Admin

---
+++ src/test/java/com/booleanuk/core/AdminTest.java
@@ -0,0 +1,12 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class AdminTest {
+
+    @Test
+    public void testAccountInit() {
+        Admin admin = new Admin();
+
+    }
+}
---
+++ src/test/java/com/booleanuk/core/UserTest.java
@@ -0,0 +1,12 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    public void testAccountInit() {
+        User user = new User();
+
+    }
+
+}
