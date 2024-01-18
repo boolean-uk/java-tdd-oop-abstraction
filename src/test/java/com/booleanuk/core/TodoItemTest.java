f49510e4714c374872b6c3438f5389d636e494f2
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-18 09:44:38 +0100

    Added test to get methods for each attribute. Added also a get and set class to each attribute.

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void testGetTitle(){
+        TodoItem item = new TodoItem("Bread","Whole wheat","Fresh");
+        Assertions.assertEquals("Bread", item.getTitle());
+
+    }
+
+    @Test
+    public void testGetDetail(){
+        TodoItem item = new TodoItem("Bread","Whole wheat","Fresh");
+        Assertions.assertEquals("Whole wheat", item.getDetail());
+    }
+
+    @Test
+    public void testGetStatus(){
+        TodoItem item = new TodoItem("Bread","Whole wheat","Fresh");
+        Assertions.assertEquals("Fresh", item.getStatus());
+    }
+
+    @Test
+    public void testSetTitle(){
+        TodoItem item = new TodoItem("Bread","Whole wheat","Fresh");
+        item.setTitle("Coffee");
+        Assertions.assertEquals("Coffee", item.getTitle());
+
+    }
+
+
 
 }
