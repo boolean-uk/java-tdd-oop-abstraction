c0c104a7a588ebcf2344f3c1cfbb15b316e3c593
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-18 11:56:12 +0100

    exercise 1 & 2

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void testTodoItemGetNameAndDetailAndStatus(){
+        TodoItem ti = new TodoItem("Dinner", "Make dinner");
+        Assertions.assertEquals("Dinner", ti.getTitle());
+        Assertions.assertEquals("Make dinner", ti.getDetail());
+    }
 
+    @Test
+    public void testTodoItemInitialStatusSetToFalse(){
+        TodoItem ti = new TodoItem("Dinner", "Make dinner");
+        Assertions.assertFalse(ti.getStatus());
+    }
+
+    @Test
+    public void testTodoItemToggleStatusShouldBeTrue(){
+        TodoItem ti = new TodoItem("Dinner", "Make dinner");
+        ti.toggleStatus();
+        Assertions.assertTrue(ti.getStatus());
+    }
 }
