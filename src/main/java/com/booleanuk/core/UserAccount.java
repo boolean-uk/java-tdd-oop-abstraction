f594d9836a51cc344f7e19b256127a17ea8076c3
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-18 11:26:34 +0100

    Implemented methods in UserAccount.java based on tests and domain modell. Last test still failing

--- src/main/java/com/booleanuk/core/UserAccount.java
+++ src/main/java/com/booleanuk/core/UserAccount.java
@@ -14,4 +14,51 @@ public class UserAccount {
         this.loggedIn = false;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if(email.contains("@")) {
+            this.email = email;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if(password.length() >= 8) {
+            this.password = password;
+        }
+    }
+
+    public void enableAccount() {
+        this.enabled = true;
+    }
+
+    public void disableAccount() {
+        this.enabled = false;
+    }
+
+    public boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void logIn(String email, String password) {
+        if(!this.loggedIn) {
+            if (this.enabled) {
+                if(this.email.equals(email) && this.password.equals(password)) {
+                    this.loggedIn = true;
+                }
+            }
+            this.loggedIn = false;
+        }
+    }
+
+    public boolean isLoggedIn() {
+        return this.loggedIn;
+    }
+
 }
