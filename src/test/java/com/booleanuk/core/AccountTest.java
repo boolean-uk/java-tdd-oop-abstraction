cb3058c2df7bd8121a9b7804c75a53f4cce5ad57
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-18 09:28:18 +0100

    Add test for account setters and getters

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,12 +1,23 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
 
     @Test
     public void testAccountInit() {
-        Account account = new Account();
+        Account account = new Account("email@email.email", "bestPassword");
+
+        Assertions.assertEquals("invalid password", account.setPassword("1"));
+        Assertions.assertEquals("invalid email", account.setEmail("1"));
+        Assertions.assertFalse(account.getEnabled());
+        Assertions.assertEquals("invalid login", account.ableToLogin());
+        account.setEnabled(true);
+        Assertions.assertTrue(account.getEnabled());
+        Assertions.assertTrue(account.ableToLogin());
+
+
 
     }
 }
