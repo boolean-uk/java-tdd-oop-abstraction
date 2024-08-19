e7944611b137075e88adc75d12840cce4d2c7584
Iris Flystam <badde00@gmail.com>
2024-08-19 10:55:36 +0200

    GREEN: testSetDetail passes

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -30,4 +30,8 @@ public class TodoItem {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
 }
