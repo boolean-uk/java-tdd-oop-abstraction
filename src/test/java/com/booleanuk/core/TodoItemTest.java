7ceebb7c1aa48a172850b11b40bc8784fa721fe2
shyye <emma.art.dev@gmail.com>
2024-08-19 10:59:06 +0200

    Update checkStatusTest

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -7,9 +7,12 @@ class TodoItemTest {
 
     @Test
     public void checkSetStatus() {
-        TodoItem todo = new TodoItem("Todo", "JUnit testing", "pending");
-        todo.markAsComplete();
+        TodoItem todo = new TodoItem("Todo", "JUnit testing");
 
+        todo.markAsComplete();
         Assertions.assertEquals("complete", todo.getStatus());
+
+        todo.markAsIncomplete();
+        Assertions.assertEquals("incomplete", todo.getStatus());
     }
 }
