6cd43ee869f027fae12094cb6b784b3a51007762
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-19 11:23:52 +0200

    made second test fail.

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -30,8 +30,16 @@ public class Account {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setStatus(Boolean status) {
--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -1,16 +1,29 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    public Account account;
+
+    @BeforeEach
+    public void setUp() {
+        this.account = new Account("example.com", "12345");
+    }
 
     @Test
-    public void testVerifyEmail() throws Exception {
-        Account account = new Account("example.com", "12345");
+    public void testVerifyEmail() {
         Assertions.assertEquals("invalid email", account.getEmail());
+        account.setEmail("example@.com");
+        Assertions.assertEquals("example@.com", account.getEmail());
+    }
+
+    @Test
+    public void testVerifyPassword() {
+        Assertions.assertEquals("invalid password", account.getPassword());
     }
 
     @Test
