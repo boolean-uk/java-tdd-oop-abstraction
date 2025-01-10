36fb00dc0a7dea04b7578cabe136a0a49e853b51
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-10 10:28:44 +0100

    Now tests are done

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -20,4 +20,36 @@ class TodoItemTest {
 
         Assertions.assertSame("Not done", todoItem.getStatus());
     }
+
+    @Test
+    public void setTitleOnItem(){
+        TodoItem todoItem = new TodoItem("Clean", "vacuum the apartment", "Not done");
+
+        todoItem.setTitle("Wash");
+
+        Assertions.assertSame("Wash", todoItem.getTitle());
+    }
+
+    @Test
+    public void getTitleOnItem(){
+        TodoItem todoItem = new TodoItem("Run", "vacuum the apartment", "Not done");
+
+        Assertions.assertSame("Run", todoItem.getTitle());
+    }
+
+    @Test
+    public void setDetailOnItem(){
+        TodoItem todoItem = new TodoItem("Clean", "vacuum the apartment", "Not done");
+
+        todoItem.setTitle("Clean the kitchen");
+
+        Assertions.assertSame("Clean the kitchen", todoItem.getDetail());
+    }
+
+    @Test
+    public void getDetailOnItem(){
+        TodoItem todoItem = new TodoItem("Clean", "clean the floors", "Not done");
+
+        Assertions.assertSame("clean the floors", todoItem.getDetail());
+    }
 }
