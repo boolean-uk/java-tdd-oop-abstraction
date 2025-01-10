b6e9f266d6f0949b76ce21c5e4622c17185b1c8d
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-01-10 10:17:49 +0100

    Created tests

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,43 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    @Test
+    public void GetTitleTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        Assertions.assertEquals("laundry", todoList.getTitle());
+    }
+
+    @Test
+    public void SetTitleTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        todoList.setTitle("drawing");
+        Assertions.assertEquals("drawing", todoList.getTitle());
+    }
+
+    @Test
+    public void GetDetailTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        Assertions.assertEquals("wash dark clothes", todoList.getDetail());
+    }
+
+    @Test
+    public void SetDetailTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        todoList.setDetail("wash white clothes");
+        Assertions.assertEquals("wash white clothes", todoList.getDetail());
+    }
+
+    @Test
+    public void GetStatusTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        Assertions.assertEquals("incomplete", todoList.getStatus());
+    }
+
+    @Test
+    public void SetStatusTest() {
+        TodoItem todoList = new TodoItem("laundry", "wash dark clothes", "incomplete");
+        todoList.setStatus("complete");
+        Assertions.assertEquals("complete", todoList.getStatus());
+    }
+
 }
