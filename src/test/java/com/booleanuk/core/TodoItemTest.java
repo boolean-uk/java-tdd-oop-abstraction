df31c2bf29fed8d3d71bca1ebc20f5bbf4851fa9
Mattias Hedbom <mattias.hedbom@se.experis.com>
2025-01-10 10:32:11 +0100

    Tests are made

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,60 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    // Status
+    @Test
+    public void statusIsUpdated(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+        String status = "completed";
+        todoItem.setStatus(status);
+
+        Assertions.assertEquals(status, todoItem.getStatus());
+    }
+
+    @Test
+    public void statusIsRecieved(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+
+        Assertions.assertEquals("Incomplete", todoItem.getStatus());
+    }
+
+
+    // Detail
+    @Test
+    public void detailIsChanged(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+        String detail = "Clean the toilet";
+        todoItem.setDetail(detail);
+
+        Assertions.assertEquals(detail, todoItem.getDetail());
+    }
+
+    @Test
+    public void detailIsRecieved(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+
+        Assertions.assertEquals("Vacuum the livingroom", todoItem.getDetail());
+    }
+
+
+    // Title
+    @Test
+    public void titleIsChanged(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+        String title = "Clean livingroom";
+        todoItem.setTitle(title);
+
+        Assertions.assertEquals(title, todoItem.getTitle());
+    }
+
+    @Test
+    public void titleIsRecieved(){
+        TodoItem todoItem = new TodoItem("Clean room", "Vacuum the livingroom", "Incomplete");
+
+        Assertions.assertEquals("Clean room", todoItem.getTitle());
+    }
+
+
+
+
 }
