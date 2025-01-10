82765d730bb03dabf13fb53c43817cfe97e62d50
Robin Kaga <robin.kaga@no.experis.com>
2025-01-10 10:20:18 +0100

    added tests

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,31 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    @Test
+    public void testGetStatus(){
+        TodoItem todoItem = new TodoItem("Buy bread", "From store", "Incomplete");
+
+        Assertions.assertEquals("Incomplete",todoItem.getStatus());
+    }
+
+    @Test
+    public void testSetStatus(){
+        TodoItem todoItem = new TodoItem("Buy bread", "From store", "Incomplete");
+        Assertions.assertEquals("Incomplete",todoItem.getStatus());
+        todoItem.setStatus("Complete");
+        Assertions.assertEquals("Complete",todoItem.getStatus());
+    }
+
+    @Test
+    public void testGetTitle(){
+        TodoItem todoItem = new TodoItem("Buy bread", "From store", "Incomplete");
+        Assertions.assertEquals("Buy bread",todoItem.getTitle());
+    }
+
+    @Test
+    public void testGetDetail(){
+        TodoItem todoItem = new TodoItem("Buy bread", "From store", "Incomplete");
+        Assertions.assertEquals("From store",todoItem.getDetail());
+    }
+
 }
