c68bb784832e8450a041a36d58421a2b64a4b4e8
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-01-10 10:23:48 +0100

    Refactored code

--- src/main/java/com/booleanuk/core/TodoItem.java
+++ src/main/java/com/booleanuk/core/TodoItem.java
@@ -18,4 +18,21 @@ public class TodoItem {
     public String getStatus() {
         return this.status;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        if(detail.equals("complete")) this.status = detail;
+        else if(detail.equals("incomplete")) this.status = detail;
+    }
 }
