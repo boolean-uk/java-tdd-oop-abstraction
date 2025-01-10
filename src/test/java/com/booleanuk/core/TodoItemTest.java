5b825a730a1b6ea729f81cd0d52fe38f4491b743
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-10 10:24:19 +0100

    Tests done

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -5,4 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
 
+    @Test
+    public void setStatusOnItem(){
+        TodoItem todoItem = new TodoItem("Clean", "vacuum the apartment", "Not done");
+
+        todoItem.setStatus("Done");
+
+        Assertions.assertSame("Done", todoItem.getStatus());
+    }
+
+    @Test
+    public void getStatusOnItem(){
+        TodoItem todoItem = new TodoItem("Clean", "vacuum the apartment", "Not done");
+
+        Assertions.assertSame("Not done", todoItem.getStatus());
+    }
 }
